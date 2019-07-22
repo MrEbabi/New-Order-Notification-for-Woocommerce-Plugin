4bf942c2756ad0f53808710779355cfb6be99de6
Eyüp Gülşen <40281221+MrEbabi@users.noreply.github.com>
2019-07-22 12:44:30 +0300

    v1.0.3 - product ID selection feature

--- new-order-notification-for-woocommerce/new-order-notification-admin.php
+++ new-order-notification-for-woocommerce/new-order-notification-admin.php
@@ -35,10 +35,11 @@ function new_order_notification_menu()
     if(!$options)
     {
         $musicUrlMp3 = plugins_url('assets/order-music.mp3',__FILE__ );
-        $refreshTime = 10;
+        $refreshTime = 30;
         $order_header = "Order Notification - New Order";
         $order_text = "Check Details: ";
         $confirm = "ACKNOWLEDGE THIS NOTIFICATION";
+        $product_ids = array ();
         //pending = 1, on-hold = 3, processing = 5, pending&on-hold = 4, pending&processing = 6, on-hold&processing = 8, pending&on-hold&processing = 9
         $order_statuses = 9;
         
@@ -51,6 +52,7 @@ function new_order_notification_menu()
         'order_text'    =>  $order_text,
         'confirm'   =>  $confirm,
         'statuses'  =>  $order_statuses,
+        'product_ids'   =>  $product_ids,
         ));
     }
     else
@@ -61,8 +63,15 @@ function new_order_notification_menu()
         $order_text = $options['order_text'];
         $confirm = $options['confirm'];
         $order_statuses = $options['statuses'];
+        $product_ids = $options['product_ids'];
     }
     
+    $alertForThisProduct = false;
+    $isAllProducts = true;
+    if(count($product_ids) != 0)
+    {
+        $isAllProducts = false;
+    }
     $isNew = false;
     $isAnyDeletion = false;
     if(in_array($last_order[0], $options['check_deleted'])) $isAnyDeletion = true;
@@ -70,33 +79,44 @@ function new_order_notification_menu()
     if($last_order[0] != $options['last_order'] && (!$isAnyDeletion))
     {
         $lastOrder = wc_get_order( $last_order[0] );
-        echo("<script>console.log('PHP: ".$lastOrder->get_status()."');</script>");
+        if(!$isAllProducts)
+        {
+            foreach( $lastOrder->get_items() as $item_id => $item )
+            {
+                $product_id = $item->get_product_id();
+                $variation_id = $item->get_variation_id();
+                if(in_array($product_id, $product_ids) || in_array($variation_id, $product_ids))
+                {
+                    $alertForThisProduct = true;
+                }
+            }
+        }
         
-        if($options['statuses'] == 9)
+        if($options['statuses'] == 9 && ($isAllProducts || $alertForThisProduct) )
         {
             $isNew = true;
         }
-        if($options['statuses'] == 8)
+        if($options['statuses'] == 8 && ($isAllProducts || $alertForThisProduct))
         {
-            if($lastOrder->get_status() == 'on-hold' || $lastOrder->get_status() == 'processing') $isNew = true;
+            if($lastOrder->get_status() == 'on-hold' || $lastOrder->get_status() == 'processing' && ($isAllProducts || $alertForThisProduct)) $isNew = true;
         }
         if($options['statuses'] == 6)
         {
-            if($lastOrder->get_status() == 'pending' || $lastOrder->get_status() == 'processing') $isNew = true;
+            if($lastOrder->get_status() == 'pending' || $lastOrder->get_status() == 'processing' && ($isAllProducts || $alertForThisProduct)) $isNew = true;
         }
-        if($options['statuses'] == 5)
+        if($options['statuses'] == 5 && ($isAllProducts || $alertForThisProduct))
         {
-            if($lastOrder->get_status() == 'processing') $isNew = true;
+            if($lastOrder->get_status() == 'processing' ) $isNew = true;
         }
-        if($options['statuses'] == 4)
+        if($options['statuses'] == 4 && ($isAllProducts || $alertForThisProduct))
         {
             if($lastOrder->get_status() == 'pending' || $lastOrder->get_status() == 'on-hold') $isNew = true;
         }
-        if($options['statuses'] == 3)
+        if($options['statuses'] == 3 && ($isAllProducts || $alertForThisProduct))
         {
             if($lastOrder->get_status() == 'pending') $isNew = true;
         }
-        if($options['statuses'] == 1)
+        if($options['statuses'] == 1 && ($isAllProducts || $alertForThisProduct))
         {
             if($lastOrder->get_status() == 'pending') $isNew = true;
         }
@@ -106,7 +126,6 @@ function new_order_notification_menu()
     $websiteUrl .= "/wp-admin/post.php?post=";
     $websiteUrl .= $last_order[0];
     $websiteUrl .= "&action=edit";
-    
     
     if($isNew)
     {
@@ -134,8 +153,9 @@ function new_order_notification_menu()
         <?php
         
         $popupcontent = "<audio controls autoplay><source src='".esc_html($options['mp3_url'])."' type='audio/mpeg'>Your browser does not support the audio element.</audio>";
-        $popupcontent .= "<div class='popup'><div class='cnt223'><h1>".$order_header."</h1><p>".$order_text." <a href='".esc_html($websiteUrl)."' target='_blank'>".esc_html($last_order[0])."</a><br/><br/><a href='' class='close'>".$confirm."</a></p></div></div>";
+        $popupcontent .= "<div class='popup'><div class='cnt223'><h1>".esc_html($order_header)."</h1><p>".esc_html($order_text)." <a href='".esc_html($websiteUrl)."' target='_blank'>".esc_html($last_order[0])."</a><br/><br/><a href='' class='close'>".esc_html($confirm)."</a></p></div></div>";
         echo $popupcontent;
+        
         update_option('_new_order_option', array(
             'last_order'   =>  $last_order[0],
             'check_deleted' =>  $to_check_orders,
@@ -145,6 +165,7 @@ function new_order_notification_menu()
             'order_text'    =>  $order_text,
             'confirm'   =>  $confirm,
             'statuses'  =>  $order_statuses,
+            'product_ids'   =>  $product_ids,
             ));
     }
     $content = "<h1>New Order Notification for Woocommerce</h1>";
@@ -168,10 +189,11 @@ function new_order_notification_menu()
     }
     
     $content .= "</table><br><hr>";
-    
+    $content .= "<div class='settings-area'>";
     $content .= "<table id='settings-new-order-notification'>";
     $content .= "<form action='' method='post' id='notificationSettingsForm'>";
     $content .= "<tr><th><span style='font-size:18px'>Settings for Notifications</span></th></tr>";
+    $content .= "<tr><th>Alert only for orders containing products with ID: </th><th><input type='number' min=0 name='inputForProductIds' placeholder='Please enter one by one'></th></tr>";
     $content .= "<tr><th>Refresh Time (in seconds): </th><th><input type='number' min='0' step='1' name='inputForTime' placeholder='".esc_html($options['refresh_time'])."'></th></tr>";
     $content .= "<tr><th>MP3 File URL (ends with .mp3): </th><th><input type='text' name='inputForMp3' placeholder='".esc_html($options['mp3_url'])."'></th></tr>";
     $content .= "<tr><th>Notification Header: </th><th><input type='text' name='inputForHeader' placeholder='".esc_html($options['order_header'])."'></th></tr>";
@@ -205,7 +227,28 @@ function new_order_notification_menu()
     }
     $content .= wp_nonce_field('notification_settings_form', 'nonce_of_notificationSettingsForm');
     $content .= "<tr><th><input type='submit' value='Reset to Default' name='resetSettings'></th><th><input type='submit' value='Save Settings' name='saveSettings'></th></tr>";
-    $content .= "</table></form>";
+    $content .= "</form></table></div>";
+    $content .= "<div class='settings-area' style='margin-left: 5%;'>";
+    $content .= "<table id='settings-new-order-notification'><form action='' method='post' id='notificationSettingsForm'> ";
+    $content .= "<tr><th><span style='font-size:18px'>Alert for Products with IDs:</span></th></tr>";
+    $productLoop = 0;
+    if(count($product_ids))
+    {
+        while($productLoop < count($product_ids))
+        {
+            $content .= "<tr><th><input type='checkbox' value='".esc_html($options['product_ids'][$productLoop])."' name='selectProductId[]'>".esc_html($options['product_ids'][$productLoop])."</th></tr>";
+            $productLoop++;
+        }
+    }
+    else
+    {
+        $content .= "<tr><th><h3>No Product IDs provided, all products are included.</h3></th></tr>";
+    }
+    
+    
+    $content .= wp_nonce_field('notification_settings_form_2', 'nonce_of_notificationSettingsForm_2');
+    $content .= "<tr><th><input type='submit' value='Remove Selected IDs' name='removeIds'></th></tr>";
+    $content .= "</form></table></div>";
     
     $isPosted = false;
     
@@ -214,6 +257,7 @@ function new_order_notification_menu()
         if(isset($_POST['saveSettings']))
         {
             $order_statuses = 0;
+            $count_products = count($product_ids);
             if( isset($_POST['inputForTime']) && !empty($_POST['inputForTime']) ) $refreshTime = sanitize_text_field($_POST['inputForTime']);
             if( isset($_POST['inputForMp3']) && !empty($_POST['inputForMp3']) ) $musicUrlMp3 = sanitize_text_field($_POST['inputForMp3']);
             if( isset($_POST['inputForHeader']) && !empty($_POST['inputForHeader']) ) $order_header = sanitize_text_field($_POST['inputForHeader']);
@@ -222,6 +266,7 @@ function new_order_notification_menu()
             if( isset($_POST['inputForPending']) && !empty($_POST['inputForPending']) ) $order_statuses += sanitize_text_field($_POST['inputForPending']);
             if( isset($_POST['inputForOnhold']) && !empty($_POST['inputForOnhold']) ) $order_statuses += sanitize_text_field($_POST['inputForOnhold']);
             if( isset($_POST['inputForProcessing']) && !empty($_POST['inputForProcessing']) ) $order_statuses += sanitize_text_field($_POST['inputForProcessing']);
+            if( isset($_POST['inputForProductIds']) && !empty($_POST['inputForProductIds']) ) $product_ids[$count_products] = sanitize_text_field($_POST['inputForProductIds']);
             
             update_option('_new_order_option', array(
             'last_order'   =>  $last_order[0],
@@ -232,6 +277,7 @@ function new_order_notification_menu()
             'order_text'    =>  $order_text,
             'confirm'   =>  $confirm,
             'statuses'  =>  $order_statuses,
+            'product_ids'   =>  $product_ids,
             ));
             $isPosted = true;
             header("Refresh:0");
@@ -239,7 +285,7 @@ function new_order_notification_menu()
         if(isset($_POST['resetSettings']))
         {
             $musicUrlMp3 = plugins_url('assets/order-music.mp3',__FILE__ );
-            $refreshTime = 10;
+            $refreshTime = 30;
             $order_header = "Order Notification - New Order";
             $order_text = "Check Details: ";
             $confirm = "ACKNOWLEDGE THIS NOTIFICATION";
@@ -258,7 +304,36 @@ function new_order_notification_menu()
             header("Refresh:0");
         }
     }
-
+    
+    if(wp_verify_nonce($_POST['nonce_of_notificationSettingsForm_2'], 'notification_settings_form_2'))
+    {
+        if(isset($_POST['selectProductId']) && !empty($_POST['selectProductId']))
+        {
+            foreach($_POST['selectProductId'] as $checkedBox)
+            {
+                if (($key = array_search($checkedBox, $product_ids)) !== false)
+                {
+                    unset($product_ids[$key]);
+                    array_values($product_ids);
+                }                
+            }
+            
+            update_option('_new_order_option', array(
+            'last_order'   =>  $last_order[0],
+            'check_deleted' =>  $to_check_orders,
+            'refresh_time'  =>  $refreshTime,
+            'mp3_url'   =>  $musicUrlMp3,
+            'order_header'  =>  $order_header,
+            'order_text'    =>  $order_text,
+            'confirm'   =>  $confirm,
+            'statuses'  =>  $order_statuses,
+            'product_ids'   =>  $product_ids,
+            ));
+            $isPosted = true;
+            header("Refresh:0");
+        }
+    }
+    
     if(!$isNew && !$isPosted)
     {
         $time = $options['refresh_time'];
@@ -267,6 +342,4 @@ function new_order_notification_menu()
     
     echo $content;
 }
-
-
 ?>
