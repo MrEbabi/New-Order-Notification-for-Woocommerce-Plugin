4435ec734824228fd376ffdda0e67e248fe292c0
Eyüp Gülşen <40281221+MrEbabi@users.noreply.github.com>
2019-11-28 11:09:12 +0300

    Settings removed from main screen.

--- new-order-notification-for-woocommerce/new-order-notification-admin.php
+++ new-order-notification-for-woocommerce/new-order-notification-admin.php
@@ -3,7 +3,8 @@
 add_action( 'admin_menu', 'new_order_notification' );
 
 function new_order_notification() {
-    add_menu_page( 'New Order Notification', 'New Order Notification', 'manage_options' , 'new_order_notification' , 'new_order_notification_menu', 'dashicons-warning' , '153');
+    add_menu_page( 'New Order Notification', 'New Order Notification', 'manage_woocommerce' , 'new_order_notification' , 'new_order_notification_menu', 'dashicons-warning' , '54');
+	add_submenu_page('new_order_notification', 'Settings', 'Settings', 'manage_woocommerce', 'new_order_notification_settings', 'new_order_notification_settings');
 }
 
 function new_order_notification_menu()
@@ -68,7 +69,6 @@ function new_order_notification_menu()
         ) );
         $to_check_orders = $query->get_orders();
     }
-    
     
     $options = get_option('_new_order_option');
     if(!$options)
@@ -160,12 +160,12 @@ function new_order_notification_menu()
             if($lastOrder->get_status() == 'pending') $isNew = true;
         }
     }
-    /*
+    
     $websiteUrl = get_site_url();
     $websiteUrl .= "/wp-admin/post.php?post=";
     $websiteUrl .= $last_order[0];
     $websiteUrl .= "&action=edit";
-    */
+    
     if($isNew)
     {
         ?>
@@ -207,8 +207,6 @@ function new_order_notification_menu()
             'product_ids'   =>  $product_ids,
             ));
     }
-    //$else statement for $isNew will be added here to provide auto refresh with refresh_time option
-    
     $content = "<h1>New Order Notification for Woocommerce</h1>";
     $content .= "<table id='customers-new-order-notification'>";
     $content .= "<tr><th>Recent Orders</th></tr>";
@@ -229,155 +227,8 @@ function new_order_notification_menu()
         $content .= "<tr><td>".esc_html($order_id)."</td><td>".esc_html($order_date[0])." - ".esc_html($order_date[1])."</td><td>".esc_html(ucfirst($order_status))."</td><td><a href='".esc_html($order_link)."'>Order ".esc_html($order_id)."</a></td></tr>";
     }
     
-    //settings part will be developed as a separate submenu page -- begin
     $content .= "</table><br><hr>";
-    $content .= "<div class='settings-area'>";
-    $content .= "<table id='settings-new-order-notification'>";
-    $content .= "<form action='' method='post' id='notificationSettingsForm'>";
-    $content .= "<tr><th><span style='font-size:18px'>Settings for Notifications</span></th></tr>";
-    $content .= "<tr><th>Alert only for orders containing products with ID: </th><th><input type='number' min=0 name='inputForProductIds' placeholder='Please enter one by one'></th></tr>";
-    $content .= "<tr><th>Refresh Time (in seconds): </th><th><input type='number' min='0' step='1' name='inputForTime' placeholder='".esc_html($options['refresh_time'])."'></th></tr>";
-    $content .= "<tr><th>MP3 File URL (ends with .mp3): </th><th><input type='text' name='inputForMp3' placeholder='".esc_html($options['mp3_url'])."'></th></tr>";
-    $content .= "<tr><th>Notification Header: </th><th><input type='text' name='inputForHeader' placeholder='".esc_html($options['order_header'])."'></th></tr>";
-    $content .= "<tr><th>Notification Text: </th><th><input type='text' name='inputForText' placeholder='".esc_html($options['order_text'])."'></th></tr>";
-    $content .= "<tr><th>Confirmation Text: </th><th><input type='text' name='inputForConfirm' placeholder='".esc_html($options['confirm'])."'></th></tr>";
-    //pending = 1, on-hold = 3, processing = 5, pending&on-hold = 4, pending&processing = 6, on-hold&processing = 8, pending&on-hold&processing = 9
-    $content .= "<tr><th>Notification Order Statuses: </th><th>";
-    if($options['statuses'] == 1 || $options['statuses'] == 4 || $options['statuses'] == 6 || $options['statuses'] == 9)
-    {
-        $content .= "<input type='checkbox' name='inputForPending' value='1' placeholder='Pending' checked>Pending<br>";
-    }
-    else
-    {
-        $content .= "<input type='checkbox' name='inputForPending' value='1' placeholder='Pending'>Pending<br>";
-    }
-    if($options['statuses'] == 3 || $options['statuses'] == 4 || $options['statuses'] == 8 || $options['statuses'] == 9)
-    {
-        $content .= "<input type='checkbox' name='inputForOnhold' value='3' checked>On-Hold<br>";
-    }
-    else
-    {
-        $content .= "<input type='checkbox' name='inputForOnhold' value='3'>On-Hold<br>";
-    }
-    if($options['statuses'] == 5 || $options['statuses'] == 6 || $options['statuses'] == 8 || $options['statuses'] == 9)
-    {
-        $content .= "<input type='checkbox' name='inputForProcessing' value='5' placeholder='Processing' checked>Processing</th></tr>";
-    }
-    else
-    {
-        $content .= "<input type='checkbox' name='inputForProcessing' value='5' placeholder='Processing'>Processing</th></tr>";
-    }
-    $content .= wp_nonce_field('notification_settings_form', 'nonce_of_notificationSettingsForm');
-    $content .= "<tr><th><input type='submit' value='Reset to Default' name='resetSettings'></th><th><input type='submit' value='Save Settings' name='saveSettings'></th></tr>";
-    $content .= "</form></table></div>";
-    $content .= "<div class='settings-area' style='margin-left: 5%;'>";
-    $content .= "<table id='settings-new-order-notification'><form action='' method='post' id='notificationSettingsForm'> ";
-    $content .= "<tr><th><span style='font-size:18px'>Alert for Products with IDs:</span></th></tr>";
-    $productLoop = 0;
-    if(count($product_ids))
-    {
-        while($productLoop < count($product_ids))
-        {
-            $content .= "<tr><th><input type='checkbox' value='".esc_html($options['product_ids'][$productLoop])."' name='selectProductId[]'>".esc_html($options['product_ids'][$productLoop])."</th></tr>";
-            $productLoop++;
-        }
-    }
-    else
-    {
-        $content .= "<tr><th><h3>No Product IDs provided, all products are included.</h3></th></tr>";
-    }
     
-    
-    $content .= wp_nonce_field('notification_settings_form_2', 'nonce_of_notificationSettingsForm_2');
-    $content .= "<tr><th><input type='submit' value='Remove Selected IDs' name='removeIds'></th></tr>";
-    $content .= "</form></table></div>";
-    
-    $isPosted = false;
-    
-    if(wp_verify_nonce($_POST['nonce_of_notificationSettingsForm'], 'notification_settings_form'))
-    {
-        if(isset($_POST['saveSettings']))
-        {
-            $order_statuses = 0;
-            $count_products = count($product_ids);
-            if( isset($_POST['inputForTime']) && !empty($_POST['inputForTime']) ) $refreshTime = sanitize_text_field($_POST['inputForTime']);
-            if( isset($_POST['inputForMp3']) && !empty($_POST['inputForMp3']) ) $musicUrlMp3 = sanitize_text_field($_POST['inputForMp3']);
-            if( isset($_POST['inputForHeader']) && !empty($_POST['inputForHeader']) ) $order_header = sanitize_text_field($_POST['inputForHeader']);
-            if( isset($_POST['inputForText']) && !empty($_POST['inputForText']) ) $order_text = sanitize_text_field($_POST['inputForText']);
-            if( isset($_POST['inputForConfirm']) && !empty($_POST['inputForConfirm']) ) $confirm = sanitize_text_field($_POST['inputForConfirm']);
-            if( isset($_POST['inputForPending']) && !empty($_POST['inputForPending']) ) $order_statuses += sanitize_text_field($_POST['inputForPending']);
-            if( isset($_POST['inputForOnhold']) && !empty($_POST['inputForOnhold']) ) $order_statuses += sanitize_text_field($_POST['inputForOnhold']);
-            if( isset($_POST['inputForProcessing']) && !empty($_POST['inputForProcessing']) ) $order_statuses += sanitize_text_field($_POST['inputForProcessing']);
-            if( isset($_POST['inputForProductIds']) && !empty($_POST['inputForProductIds']) ) $product_ids[$count_products] = sanitize_text_field($_POST['inputForProductIds']);
-            
-            update_option('_new_order_option', array(
-            'last_order'   =>  $last_order[0],
-            'check_deleted' =>  $to_check_orders,
-            'refresh_time'  =>  $refreshTime,
-            'mp3_url'   =>  $musicUrlMp3,
-            'order_header'  =>  $order_header,
-            'order_text'    =>  $order_text,
-            'confirm'   =>  $confirm,
-            'statuses'  =>  $order_statuses,
-            'product_ids'   =>  $product_ids,
-            ));
-            $isPosted = true;
-            header("Refresh:0");
-        }
-        if(isset($_POST['resetSettings']))
-        {
-            $musicUrlMp3 = plugins_url('assets/order-music.mp3',__FILE__ );
-            $refreshTime = 30;
-            $order_header = "Order Notification - New Order";
-            $order_text = "Check Details: ";
-            $confirm = "ACKNOWLEDGE THIS NOTIFICATION";
-            
-            update_option('_new_order_option', array(
-            'last_order'   =>  $last_order[0],
-            'check_deleted' =>  $to_check_orders,
-            'refresh_time'  =>  $refreshTime,
-            'mp3_url'   =>  $musicUrlMp3,
-            'order_header'  =>  $order_header,
-            'order_text'    =>  $order_text,
-            'confirm'   =>  $confirm,
-            'statuses'  =>  9,
-            ));
-            $isPosted = true;
-            header("Refresh:0");
-        }
-    }
-    
-    if(wp_verify_nonce($_POST['nonce_of_notificationSettingsForm_2'], 'notification_settings_form_2'))
-    {
-        if(isset($_POST['selectProductId']) && !empty($_POST['selectProductId']))
-        {
-            foreach($_POST['selectProductId'] as $checkedBox)
-            {
-                if (($key = array_search($checkedBox, $product_ids)) !== false)
-                {
-                    unset($product_ids[$key]);
-                    array_values($product_ids);
-                }                
-            }
-            
-            update_option('_new_order_option', array(
-            'last_order'   =>  $last_order[0],
-            'check_deleted' =>  $to_check_orders,
-            'refresh_time'  =>  $refreshTime,
-            'mp3_url'   =>  $musicUrlMp3,
-            'order_header'  =>  $order_header,
-            'order_text'    =>  $order_text,
-            'confirm'   =>  $confirm,
-            'statuses'  =>  $order_statuses,
-            'product_ids'   =>  $product_ids,
-            ));
-            $isPosted = true;
-            header("Refresh:0");
-        }
-    }
-    //settings part will be developed as a separate submenu page -- end
-    
-    //this if statement code block will be removed after else statement development for $isNew above
     if(!$isNew && !$isPosted)
     {
         $time = $options['refresh_time'];
@@ -386,4 +237,5 @@ function new_order_notification_menu()
     
     echo $content;
 }
+
 ?>
